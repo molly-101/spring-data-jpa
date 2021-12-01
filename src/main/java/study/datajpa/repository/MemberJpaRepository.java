6e90e3d6ff9e85d930cbdd0cab2fef94dda04ab5
onAuspicious <wotj7687@icloud.com>
2021-12-01 23:09:25 +0900

    gradle 기본 설정, 예제 도메인 추가

---
+++ src/main/java/study/datajpa/repository/MemberJpaRepository.java
@@ -0,0 +1,33 @@
+package study.datajpa.repository;
+
+import org.springframework.stereotype.Repository;
+import study.datajpa.entity.Member;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+public class MemberJpaRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public Member save(Member member) {
+        em.persist(member);
+        return member;
+    }
+
+    public void delete(Member member) {
+        em.remove(member);
+    }
+
+    public List<Member> findAll() {
+        // JPQL
+        return em.createQuery("select m from Member m", Member.class).getResultList();
+    }
+
+    public Member find(Long id) {
+        return em.find(Member.class, id);
+    }
+}
